f76c0cb319b49edfcf7e28f30f1e577712f8ab6a
Thomas Spitaler <tommy.spitaler@gmail.com>
2019-08-16 23:08:43 +0200

    add scroll reveal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './scripts/navbar'
 import './scripts/slideshow'
 import './scripts/map'
+import './scripts/scrollreveal'
 
 import './styles/styles.scss'
---
+++ src/scripts/scrollreveal.js
@@ -0,0 +1,12 @@
+import ScrollReveal from 'scrollreveal'
+
+document.addEventListener('DOMContentLoaded', init)
+
+function init() {
+    const config = {
+        easing: 'ease',
+        duration: 500,
+        scale: 0.5
+    };
+    ScrollReveal().reveal('.page-homepage__services .card', config)
+}
